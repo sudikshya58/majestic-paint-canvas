23b8ace251490fa4146b7eec272615a3f87e5ffd
Sudikshya <sudikshyakafle2058@gmail.com>
2025-08-02 21:32:32 +0545

    change package json

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: process.env.VITE_BASE_PATH || "/majestic-paint-canvas",
+  base: process.env.VITE_BASE_PATH || "/",
   server: {
     host: "::",
     port: 8081,
